6d3df28003034f752c2ecdea88507e577bf34db0
Dimas Darfiangga <dimass.darfiangga@gmail.com>
2020-07-04 11:53:35 +0700

    [UPDATE] Update Readme

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # daily-reminder-telegram-bot
+
+Telegram BOT by dimsky.
+- Daily Reminder 
+ ### Technology
+
+Uses a number of open source projects to work properly:
+
+* [Node.js Telegram Bot API](https://reactnative.dev/) - (0.50.0) - CNode.js module to interact with the official Telegram Bot API.
+* [Cron](https://www.npmjs.com/package/cron) - (1.8.2) - RCron is a tool that allows you to execute something on a schedule.
+
+Install the dependencies and start the server.
+
+```sh
+$ npm install
+$ node start
+```
---
+++ index.js
@@ -0,0 +1,53 @@
+var TelegramBot = require('node-telegram-bot-api'),
+    bot = new TelegramBot("1354607683:AAGPe_AdYNi2RgxlD0xhId8p7hGlUtMkxdI", { polling: true });
+
+var CronJob = require('cron').CronJob;
+
+let globalID = null
+
+bot.onText(/\/start/, (msg, match) => {
+  const chatId = msg.chat.id;
+  globalID = chatId
+  bot.sendMessage(chatId, 
+    'Lapor! Siap jaga IGOID 24/7! \nGunakan /help untuk lebih lanjut', {parse_mode : "HTML"});
+});
+
+bot.onText(/\/help/, (msg, match) => {
+  const chatId = msg.chat.id;
+  bot.sendMessage(chatId, 'Siap!', {
+    "reply_markup": {
+        "keyboard": [["General Meeting ID"]]
+        }
+    });
+});
+
+bot.onText(/\/update/, (msg, match) => {
+  const chatId = msg.chat.id;
+  globalID = chatId
+  bot.sendMessage(chatId, 
+    'Siap! <b>SATPAM</b> berhasil di update!', {parse_mode : "HTML"});
+});
+
+bot.on('message', (msg) => {
+    const meetingid = "General Meeting ID";
+    const chatId = msg.chat.id;
+    if (msg.text.toString().indexOf(meetingid) === 0) {
+        
+        bot.sendMessage(chatId, 
+            'Meeting ID : 233179780 \nPassword : Ab025978', {parse_mode : "HTML"});
+    }
+})
+
+var CronJob = require('cron').CronJob;
+var job = new CronJob('00 30 09 * * 1-5', function() {
+    bot.sendMessage(globalID, 
+        'Semangat Pagi! Jangan lupa update <a href=\"http://www.trello.com/\"><b>Trello</b></a>!\nGeser kerjaan kamu hari ini ke card <b>working</b> ya!', {parse_mode : "HTML"});
+}, null, true, 'Asia/Jakarta');
+job.start();
+
+var CronJob = require('cron').CronJob;
+var job = new CronJob('00 30 16 * * 1-5', function() {
+    bot.sendMessage(globalID, 
+        'Semangat Sore! Jangan lupa update <a href=\"http://www.trello.com/\"><b>Trello</b></a>!\nGeser kerjaan kamu yang sudah selesai hari ini ke card <b>QA</b> atau <b>Comment</b> kerjaan kamu jika masih dalam progress ya!', {parse_mode : "HTML"});
+}, null, true, 'Asia/Jakarta');
+job.start();
